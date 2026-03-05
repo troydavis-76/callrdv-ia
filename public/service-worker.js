5858307f0de2b136734a6235aa681562ed5c0450
troydavis-76 <yassop76@hotmail.fr>
2026-03-05 17:10:48 +0100

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'callrdv-ia-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/js/main.chunk.js',
+  '/static/js/bundle.js',
+  '/static/css/main.chunk.css',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request).catch(() => caches.match('/'));
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
